142c4ad6ba7973ff9f33cb4d84df0728c8fcfcc8
luisedua <luisedua@student.42sp.org.br>
2022-04-16 23:12:30 +0200

    C 04 [In progress...]

---
+++ C Exercises/C 04 [In progress...]/ex02/ft_putnbr.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luisedua <luisedua@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/16 23:08:13 by luisedua          #+#    #+#             */
+/*   Updated: 2022/04/16 23:08:57 by luisedua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
+}
+
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putnbr(int nb);
+
+int	main(void)
+{
+	write(1, "-1: ", 4);
+	ft_putnbr(-1);
+	write(1, "\n1: ", 4);
+	ft_putnbr(1);
+	write(1, "\n-10: ", 6);
+	ft_putnbr(-10);
+	write(1, "\n10: ", 5);
+	ft_putnbr(10);
+	write(1, "\nINT_MAX: ", 10);
+	ft_putnbr(INT_MAX);
+	write(1, "\nINT_MIN: ", 10);
+	ft_putnbr(INT_MIN);
+	write(1, "\n", 1);
+	return (0);
+}
